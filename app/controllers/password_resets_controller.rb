6e73721ff418b463146b8c5b24efa2072358aed0
Jared Melnyk <jared.melnyk@gmail.com>
2026-03-04 20:53:37 -0600

    feat: password reset edit/update and set new password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,7 @@
 
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login, only: [ :new, :create, :edit, :update ]
+  before_action :set_user_by_token, only: [ :edit, :update ]
 
   def new
   end
@@ -14,5 +15,29 @@ class PasswordResetsController < ApplicationController
       UserMailer.password_reset(user, raw_token).deliver_later
     end
     redirect_to login_path, notice: "If an account exists for that email, we've sent a link to reset your password. Check your inbox and spam."
+  end
+
+  def edit
+    return redirect_to login_path, alert: "That link is invalid or has expired." if @user.blank?
+  end
+
+  def update
+    return redirect_to login_path, alert: "That link is invalid or has expired." if @user.blank?
+    if @user.update(update_params)
+      @user.clear_password_reset!
+      redirect_to login_path, notice: "Password updated. Sign in with your new password."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user_by_token
+    @user = User.where.not(password_reset_token_digest: nil).find { |u| u.password_reset_token_valid?(params[:token]) }
+  end
+
+  def update_params
+    params.permit(:password, :password_confirmation)
   end
 end
